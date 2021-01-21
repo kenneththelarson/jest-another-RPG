792081ee31749bdc50b8c6d603486c2047c5eee2
Kenny <kenneththelarson@gmail.com>
2021-01-20 23:17:15 -0700

    create a player object

--- __tests__/Potion.test.js
+++ __tests__/Potion.test.js
@@ -1,6 +1,6 @@
 const Potion = require('../lib/Potion');
 
-test('creates a health potion object', () => {
+test('creates a random potion object', () => {
     const potion = new Potion();
 
     expect(potion.name).toEqual(expect.any(String));
